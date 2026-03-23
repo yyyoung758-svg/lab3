af5c84c547b9dec649156d22fcb263e5b2a1595d
araragi_18 <araragi_18@DESKTOP-1V23DTK>
2026-03-23 23:37:30 +0200

    Add project files.

---
+++ lab3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using VectorLib;
+
+namespace VectorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            Vector v1 = new Vector(1, 2);
+            Vector v2 = new Vector(3, 4);
+            Vector v3 = new Vector(v2);
+
+          
+            Vector v2Scaled = v2 * 2;
+
+
+            Vector result = Vector.Subtract(v3, v2);
+
+            Console.WriteLine("v1: " + v1.GetInfo());
+            Console.WriteLine("v2: " + v2.GetInfo());
+            Console.WriteLine("v3 (copy v2): " + v3.GetInfo());
+
+            Console.WriteLine("\nv2 * 2 = " + v2Scaled.GetInfo());
+
+            Console.WriteLine("v3 - v2 = " + result.GetInfo());
+
+            Console.WriteLine("length v1: " + v1.Length());
+        }
+    }
+}
---
+++ lab3/Vector.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace VectorLib
+{
+    public class Vector
+    {
+        private double x, y;
+
+
+        public double X => x;
+        public double Y => y;
+
+    
+        public Vector()
+        {
+            x = 0;
+            y = 0;
+        }
+
+    
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    
+        public Vector(Vector other)
+        {
+            x = other.x;
+            y = other.y;
+        }
+
+
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.x + b.x, a.y + b.y);
+        }
+
+
+        public static Vector operator *(Vector v, double k)
+        {
+            return new Vector(v.x * k, v.y * k);
+        }
+
+
+        public static Vector Subtract(Vector first, params Vector[] others)
+        {
+            double resX = first.x;
+            double resY = first.y;
+
+            foreach (var v in others)
+            {
+                resX -= v.x;
+                resY -= v.y;
+            }
+
+            return new Vector(resX, resY);
+        }
+
+   
+        public string GetInfo()
+        {
+            return $"({x}, {y})";
+        }
+    }
+}
